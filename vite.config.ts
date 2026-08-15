17e5dc3de85008bb76cfc50baee9e9197c0239a0
beendev21 <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>
2026-08-15 17:38:32 -0300

    configura deploy na Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), tanstackStart(), react()],
+  plugins: [
+    tailwindcss(),
+    tanstackStart(),
+    nitro({ preset: "vercel" }),
+    react(),
+  ],
 });
